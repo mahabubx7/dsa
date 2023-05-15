c242255baae1d02483d801754eaca4d8be1cd730
Mahabub Alam <mahabubx7@gmail.com>
2023-05-16 03:18:58 +0600

    :hammer: array:ts-array-list-added

--- src/typescript/array/ArrayList.ts
+++ src/typescript/array/ArrayList.ts
@@ -4,14 +4,38 @@
  * A sequence of similar data type collections.
  *--------------------------------------------*/
 
-export class ArrayList<Type> {
+export default class ArrayList<Type> {
   private list: Type[];
 
-  constructor(list = []) {
+  constructor(list: Type[] = []) {
     this.list = list;
   }
 
-  size() {
+  size(): number {
     return this.list.length;
+  }
+
+  get(index?: number): Type | Type[] {
+    if (index) return this.list[index];
+    return [...this.list];
+  }
+
+  add(item: Type): void {
+    this.list.push(item);
+  }
+
+  addAt(index: number, ...arr: Type[]): void {
+    this.list.splice(index, 0, ...arr);
+  }
+
+  pop(): Type | undefined {
+    return this.list.pop();
+  }
+
+  remove(index?: number, count: number = 1) {
+    if (index !== 0 && !index) this.pop();
+    else {
+      this.list.splice(index, count);
+    }
   }
 }
