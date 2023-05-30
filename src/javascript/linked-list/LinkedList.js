0faead4db712a9d2dec97ae186d794852860bfeb
Mahabub Alam <mahabubx7@gmail.com>
2023-05-30 23:09:21 +0600

    :hammer: linked-list/remove() --added

--- src/javascript/linked-list/LinkedList.js
+++ src/javascript/linked-list/LinkedList.js
@@ -1,5 +1,6 @@
 /**-------------------------------------------*
  * @LinkedList Source Code
+ * @type Singly LinkedList
  *--------------------------------------------*/
 
 class Node {
@@ -33,6 +34,38 @@ export default class LinkedList {
     return;
   }
 
+  remove(index = null) {
+    if (typeof index === 'number' && index < 0) {
+      return Error('invalid position or index number.');
+    }
+    if (typeof index === 'number' && index === 0) {
+      this.head = this.head.next;
+      return;
+    }
+    if (typeof index === 'number' && index > 0) {
+      let current = this.head;
+      let i = 0,
+        prev = null;
+      while (index > i) {
+        i += 1;
+        prev = current;
+        current = current.next;
+      }
+      prev.next = current.next;
+      return;
+    }
+
+    let current = this.head,
+      prev = null;
+    while (current.next !== null) {
+      prev = current;
+      current = current.next;
+    }
+    this.tail = prev;
+    prev.next = null;
+    return;
+  }
+
   getSize() {
     return this.size;
   }
@@ -40,6 +73,7 @@ export default class LinkedList {
   print() {
     let result = '';
     let current = this.head;
+    if (current === null) return 'empty linked-list!';
     while (current.next !== null) {
       result += `${current.data} -> `;
       current = current.next;
--- src/javascript/linked-list/LinkedList.test.js
+++ src/javascript/linked-list/LinkedList.test.js
@@ -19,4 +19,23 @@ describe('Tests for LinkedList', () => {
     expect(ll.getSize()).toStrictEqual(3);
     expect(ll.print()).toBe('7 -> 10 -> 1');
   });
+
+  it('should be able to remove nodes from linked-list', () => {
+    const ll = new LinkedList();
+    ll.add(7);
+    ll.add(5);
+    ll.add(0);
+    ll.add(2);
+    ll.add(3);
+    expect(ll.getSize()).toEqual(5);
+    expect(ll.print()).toBe('7 -> 5 -> 0 -> 2 -> 3');
+    ll.remove(); // by default, removes the last item
+    expect(ll.print()).toBe('7 -> 5 -> 0 -> 2');
+    ll.remove(0); // removes from the first-index
+    expect(ll.print()).toBe('5 -> 0 -> 2');
+    ll.remove(1); // removed from specific position
+    expect(ll.print()).toBe('5 -> 2');
+    ll.remove(1); // removed from the last-index
+    expect(ll.print()).toBe('5');
+  });
 });
